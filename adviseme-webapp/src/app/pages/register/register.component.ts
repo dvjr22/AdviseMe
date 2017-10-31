3cbaf994b94d3d5a7fd810d5e4346d61634189d1
Tyler Moon <moon.tyler@gmail.com>
2017-10-30 21:58:25 -0400

    Updated registration component to new user profile

--- adviseme-webapp/src/app/pages/register/register.component.ts
+++ adviseme-webapp/src/app/pages/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  model: any = {};
+  loading = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
-  ngOnInit() {
+
+  register() {
+    this.loading = true;
+    this.userService.create(this.model)
+      .subscribe(
+        data => {
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.loading = false;
+          console.error('Error registering');
+        });
   }
-
 }
