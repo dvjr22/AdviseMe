8b6c74fa5d744a31de172d7bb7940ee13c3ea5c5
Tyler Moon <moon.tyler@gmail.com>
2017-11-03 11:14:09 -0400

    Added a logout component so that you can logout without manually deleteing the local storage

--- adviseme-webapp/src/app/app-routing.module.ts
+++ adviseme-webapp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { AdvisementSignupComponent } from './pages/advisement-signup/advisement-signup.component';
 import { AppointmentComponent } from './pages/appointment/appointment.component';
 import { ClassInfoComponent } from './pages/class-info/class-info.component';
@@ -9,8 +10,12 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LogoutComponent } from './pages/logout/logout.component';
+
+// Services
 import { CanActivateUser } from './services/authentication.service';
 
+// Definitions for possible routes and the components to load in the router
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full', canActivate: [CanActivateUser]},
   { path: 'advisement-signup', component: AdvisementSignupComponent, canActivate: [CanActivateUser]},
@@ -20,9 +25,11 @@ const routes: Routes = [
   { path: 'dashboard',  component: DashboardComponent, canActivate: [CanActivateUser]},
   { path: 'login', component: LoginComponent},
   { path: 'profile', component: ProfileComponent, canActivate: [CanActivateUser]},
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'logout', component: LogoutComponent }
 ];
 
+// Initialize the routes defined in routes above
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
