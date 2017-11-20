f4b5e62d1a257520d20f065741b9be828e7feccf
valdeslab <dvjr22@gmail.com>
2017-11-20 15:29:19 -0500

    working on data insertion

--- adviseme-database/Build/Python/insert.py
+++ adviseme-database/Build/Python/insert.py
@@ -1,13 +1,45 @@
-import pymongo
-import collections
+# connection.js
+# @author Diego Valdes
+# Nov. 20, 2016
+
+import pymongo, collections, os, sys, csv
 
 # Get Mongo db connection
 client = pymongo.MongoClient('localhost', 27017)
 # Get dbName.collectionName
-db = client.adviseMe.classes	
+db = client.adviseMe.classes
+
+path = '/home/valdeslab/SeniorYear/AdviseMe/AdviseMe/adviseme-database/Data/SemiStructured'
+
+# Walk directory path for .csv files containing class data
+for root, subdirs, files in os.walk(path):
+
+	for classFiles in os.listdir(root):
+		
+		# Create path of file
+		filePath = os.path.join(root, classFiles)
+
+		# Check if path is to directory, pass to next path (This should never happen)
+		if os.path.isdir(filePath):
+			pass
+
+		# Process file
+		else:
+			print(filePath)
+
+			with open(filePath, 'r') as file:
+
+				spamreader = csv.reader(file)
+
+				for row in spamreader:
+					print(row)
+
+
+
 
 print(db)
 
+'''
 # Create 
 data = collections.OrderedDict()
 data['_id'] = 'CSCE200'
@@ -19,3 +51,4 @@ data['curriculum'] = ['cs', 'ce']
 
 # Insert data
 db.insert_one(data)
+'''
