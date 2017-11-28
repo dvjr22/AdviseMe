e910817a0abeee08c142e883ab25f56aecbfe573
tbhall <tbhall@email.sc.edu>
2017-11-27 19:49:03 -0500

    added to user profile

--- adviseme-webapp/src/app/_shared/models/user.ts
+++ adviseme-webapp/src/app/_shared/models/user.ts
@@ -5,6 +5,28 @@ enum Role {
   Advisor, //1
   Admin,   //2
 }
+
+enum Status {
+  Freshman, //0
+  Sophmore, //1
+  Junior,   //2
+  Senior,   //3
+  SSenior,  //4 Super Senior
+}
+
+enum Major {
+  AerospaceEng,     //0
+  BiomedicalEng,    //1
+  ChemicalEng,      //2
+  CivilEng,         //3
+  EnvironmentalEng, //4
+  ComputerScience,  //5
+  ComputerEng,      //6
+  ElectricalEng,    //7
+  InfoTech,         //8
+  MechanicalEng,    //9
+  NuclearEng,       //10
+}
 // authentication user profile
 @Injectable()
 export class User {
@@ -17,4 +39,8 @@ export class User {
   email: string;
   university: string;
   role: Role;
+
+  //Student Role
+  status: Status;
+  major: Major;
 }
