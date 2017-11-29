1a8f57631ae35553c7378430b25c179289d076ca
tbhall <tbhall@email.sc.edu>
2017-11-29 09:24:41 -0500

    dropdown status

--- adviseme-webapp/src/app/_shared/models/user.ts
+++ adviseme-webapp/src/app/_shared/models/user.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
 
-enum Role {
-  Student, //0
-  Advisor, //1
-  Admin,   //2
+export enum Role {
+  None,
+  Student, // 0
+  Advisor, // 1
+  Admin,   // 2
 }
 
-enum Status {
-  Freshman, //0
-  Sophmore, //1
-  Junior,   //2
-  Senior,   //3
-  SSenior,  //4 Super Senior
+export enum Status {
+  None,
+  Freshman, // 0
+  Sophmore, // 1
+  Junior,   // 2
+  Senior,   // 3
+  SSenior,  // 4 Super Senior
 }
 
-enum Major {
-  AerospaceEng,     //0
-  BiomedicalEng,    //1
-  ChemicalEng,      //2
-  CivilEng,         //3
-  EnvironmentalEng, //4
-  ComputerScience,  //5
-  ComputerEng,      //6
-  ElectricalEng,    //7
-  InfoTech,         //8
-  MechanicalEng,    //9
-  NuclearEng,       //10
+export enum Major {
+  None,
+  AerospaceEng,     // 0
+  BiomedicalEng,    // 1
+  ChemicalEng,      // 2
+  CivilEng,         // 3
+  EnvironmentalEng, // 4
+  ComputerScience,  // 5
+  ComputerEng,      // 6
+  ElectricalEng,    // 7
+  InfoTech,         // 8
+  MechanicalEng,    // 9
+  NuclearEng,       // 10
 }
 // authentication user profile
 @Injectable()
@@ -40,7 +43,7 @@ export class User {
   university: string;
   role: Role;
 
-  //Student Role
+  // Student Role
   status: Status;
   major: Major;
 }
--- adviseme-webapp/src/app/pages/profile/edit-profile/edit-profile.component.ts
+++ adviseme-webapp/src/app/pages/profile/edit-profile/edit-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from '../../../_shared/models/user';
+import { User, Status } from '../../../_shared/models/user';
 import { UserService } from '../../../_shared/services/user.service';
 
 @Component({
@@ -8,13 +8,18 @@ import { UserService } from '../../../_shared/services/user.service';
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss'],
 })
+
+
 export class EditProfileComponent implements OnInit {
+  status = Status;
+  keys: any;
   currentUser: User;
   user: any = {};
   emailBegin: string;
   emailEnding: string;
 
   constructor(private userService: UserService) {
+    this.keys = Object.keys(this.status).filter(Number);
   }
 
   ngOnInit() {
@@ -27,6 +32,6 @@ export class EditProfileComponent implements OnInit {
     this.currentUser.fullName = this.currentUser.firstName + ' ' + this.currentUser.lastName;
     this.currentUser.email = this.emailBegin + this.emailEnding;
     this.userService.update(this.currentUser).subscribe();
-    //router to Profile
+    // router to Profile
   }
 }
