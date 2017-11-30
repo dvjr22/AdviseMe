12a75f54c2b19d230f4d34d58087d6119d8aaf1c
Tyler Moon <moon.tyler@gmail.com>
2017-11-30 13:40:35 -0500

    Started a unit test

---
+++ adviseme-api/test/user.test.js
@@ -0,0 +1,32 @@
+let mongoose = require("mongoose");
+let User = require('../models/user.model');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../api');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+// Parent block
+describe('User', () => {
+  beforeEach((done) => {
+    // Actions to be done before each test
+    console.log("Starting test");
+  })
+})
+
+/*
+ * Test the /get route
+ */
+ describe(' /GET all users', () => {
+   it('it should GET all the users', (done) => {
+     chai.request(server)
+      .get('/users')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        done();
+      });
+   });
+ });
