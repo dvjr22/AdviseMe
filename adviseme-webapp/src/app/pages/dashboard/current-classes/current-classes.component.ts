ed565fadb6b8951ef1349bd7c1bcbc449ab6eab6
tbhall <tbhall@email.sc.edu>
2017-11-30 08:55:55 -0500

    smart table up, modified user to contain array of courses

--- adviseme-webapp/src/app/pages/dashboard/current-classes/current-classes.component.ts
+++ adviseme-webapp/src/app/pages/dashboard/current-classes/current-classes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalDataSource } from 'ng2-smart-table';
+
 import { User } from '../../../_shared/models/user';
 import { UserService } from '../../../_shared/services/user.service';
 
@@ -14,6 +16,23 @@ import {CapitalizePipe} from '../../../@theme/pipes/capitalize.pipe';
 export class CurrentClassesComponent implements OnInit {
 
   currentUser: User;
+  data: any;
+  settings = {
+    actions: false,
+    columns: {
+      prefix: {
+        title: 'Prefix',
+      },
+      coNum: {
+        title: 'Course Num',
+      },
+      grade: {
+        title: 'Grade',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
 
   constructor(private userService: UserService) {
   }
@@ -21,6 +40,10 @@ export class CurrentClassesComponent implements OnInit {
   ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.userService.getById(this.currentUser._id)
-        .subscribe(res => this.currentUser = res);
+        .subscribe(res => {
+          this.currentUser = res;
+          this.data = this.currentUser.course;
+          this.source.load(this.data);
+        });
   }
 }
--- adviseme-webapp/src/app/pages/dashboard/dashboard.module.ts
+++ adviseme-webapp/src/app/pages/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AngularEchartsModule } from 'ngx-echarts';
-
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { StatusCardComponent } from './status-card/status-card.component';
@@ -10,6 +10,7 @@ import { CurrentClassesComponent } from './current-classes/current-classes.compo
   imports: [
     ThemeModule,
     AngularEchartsModule,
+    Ng2SmartTableModule,
   ],
   declarations: [
     DashboardComponent,
