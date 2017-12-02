3c72672557480cd6e46891e6da60dd68bb868286
Ethan Harmon <ethanharmon2018@gmail.com>
2017-12-01 23:41:37 -0500

    We need to create a master user that is enrolled in every class so that we can just reuse code

--- adviseme-webapp/src/app/pages/classes/courses/courses.component.ts
+++ adviseme-webapp/src/app/pages/classes/courses/courses.component.ts
@@ -11,23 +11,25 @@ import { ClassService } from '../../../_shared/services/class.service';
   //styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-    classes: any = [];
-    class: any = classes.class;
+
+    classes: Class[];
+    Class: any;
+    courses: any = [{}];
     settings = {
       actions: false,
       columns: {
-        department: {
+        prefix: {
           title: 'Department',
         },
-        class.courseNo: {
+        courseNo: {
           title: 'Course Number',
         },
-        class.title: {
-          title: 'Title'
+        title: {
+          title: 'Title',
         },
         prerequisites: {
-          title: 'Prerequisites'
-        }
+          title: 'Prerequisites',
+        },
       },
     };
 
@@ -41,7 +43,13 @@ export class CoursesComponent implements OnInit {
       this.classService.getClasses()
         .subscribe(res => {
           this.classes = res;
-          this.source.load(this.classes);
+          for (let i = 0; i < this.classes.class.length; i++) {
+            const prefix = this.classes.class[i].prefix;
+            const title = this.classes.class[i].title;
+            const courseNo = this.classes.class[i].courseNo;
+            this.class.push({prefix: prefix, title: title, courseNo: courseNo});
+          }
+          this.source.load(this.class);
         });
     }
 }
