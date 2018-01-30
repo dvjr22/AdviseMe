457937e790b969b03a8215d264892d24151ceee1
tbhall <tbhall@email.sc.edu>
2018-01-30 15:06:33 -0500

    ckeditor removed from file

--- adviseme-webapp/src/app/pages/classes/classes.module.ts
+++ adviseme-webapp/src/app/pages/classes/classes.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CKEditorModule } from 'ng2-ckeditor';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
@@ -10,7 +9,6 @@ import { ClassesRoutingModule, routedComponents } from './classes-routing.module
   imports: [
     ThemeModule,
     ClassesRoutingModule,
-    CKEditorModule,
     Ng2SmartTableModule,
   ],
   declarations: [
