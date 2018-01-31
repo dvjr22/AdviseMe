d8dc5baa6287a44948704e50d7258bd7a4cff92f
valdeslab <dvjr22@gmail.com>
2018-01-31 12:07:49 -0500

    Nara connects to db

--- adviseme-database/Nara/Nara.js
+++ adviseme-database/Nara/Nara.js
@@ -13,6 +13,9 @@ class Nara {
 		this.studentId = studentId;
 		//this.MongoClient = require('mongodb').MongoClient;	// Mongo client to connect
 		this.url = "mongodb://localhost:27017/adviseMe";	
+
+		this.MongoClient = require('mongodb').MongoClient;
+		//this.assert = require('assert');
 	}
 
 	get studentId(){
@@ -26,24 +29,32 @@ class Nara {
 
 	getThatRec(){
 		
-		this.log("getting that recomendation for " + this.studentId);
-
 		//get the results here
+
+		// Connect to db
+		this.MongoClient.connect(this.url, function(err, db) {
+
+			if (!err) {
+				console.log("connected");
+
+				// Stuff to do goes in here
+
+				db.collection('users').findOne({ studentID : 'tHall01'}, function(err, doc){
+					console.log(doc);
+				});
+
+			}
+
+			db.close();
+
+		});
 
 		var results = ["CSCE145", "CSCE146", "MATH141"];
 
 		return results;
 	}
 
-	log(statement) {
-		console.log(statement);
-	}
-
 }
 
 module.exports = Nara;
 
-
-
-
-//console.log(doWorkNara("A1234"));
--- adviseme-database/Nara/NaraTester.js
+++ adviseme-database/Nara/NaraTester.js
@@ -16,15 +16,19 @@ function iterate(list) {
 }
 
 //Test v1
-const Nara = require('/home/diego/Nara.js');
-let nara = new Nara("A1234");
+//const Nara = require('/home/diego/Nara.js'); 
+const Nara = require('/home/valdeslab/SeniorYear/AdviseMe/AdviseMe/adviseme-database/Nara/Nara.js');
+let nara = new Nara("tHall01");
 //log(nara.studentId);
 let results_1 = nara.getThatRec();
 iterate(results_1);
 
-
+/*
 //Test v2
-const NaraV2 = require('/home/diego/NaraV2.js');
+//const NaraV2 = require('/home/diego/NaraV2.js');
+const NaraV2 = require('/home/valdeslab/SeniorYear/AdviseMe/AdviseMe/adviseme-database/Nara/Nara.js');
 let narav2 = new NaraV2();
 let results_2 = narav2.getThatRec("A5678");
 iterate(results_2);
+'/home/valdeslab/SeniorYear/AdviseMe/AdviseMe/adviseme-database/Nara/Nara.js'
+*/
