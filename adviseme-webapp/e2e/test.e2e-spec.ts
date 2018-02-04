59d9e7683845f7bb35c8dedf6c555832b30f087a
Tyler Moon <moon.tyler@gmail.com>
2018-02-04 10:16:35 -0500

    Fixing unit tests

--- adviseme-webapp/e2e/test.e2e-spec.ts
+++ adviseme-webapp/e2e/test.e2e-spec.ts
@@ -26,26 +26,4 @@ describe('Login System', () => {
     // Expect the toast to appear
     expect(element(by.css('.toast')).isPresent()).toBeTruthy('The invalid login toast should appear');
   });
-
-  it('should create a new login', () => {
-    // Navigate to the registration
-    browser.waitForAngularEnabled(true);
-    element(by.id('signuplink')).click();
-    expect(element(by.id('input-email')).isPresent()).toBeTruthy('The email field should appear to denote the registration screen');
-    browser.waitForAngularEnabled(false);
-    // Fill out the registration form
-    element(by.id('input-username')).sendKeys('testuser' + Date.now());
-    element(by.id('input-firstName')).sendKeys('Test');
-    element(by.id('input-lastName')).sendKeys('User');
-    element(by.id('input-email')).sendKeys('testuser@email.com');
-    element(by.id('input-password')).sendKeys('test');
-    element(by.id('input-re-password')).sendKeys('test');
-    element(by.id('tocbox')).click();
-
-    // Register test user
-    element(by.css('.btn-block')).click();
-
-    // Check that the redirect to the login screen worked
-    expect(element(by.id('input-email')).isPresent()).toBeFalsy('The email field should not appear to denote the login screen');
-  });
 });
