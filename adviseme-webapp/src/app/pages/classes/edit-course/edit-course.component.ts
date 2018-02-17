fe179386647715ba9842d92ad86fdcf47dd6dd13
tbhall <tbhall@email.sc.edu>
2018-02-17 18:21:31 -0500

    able to collect all of new data to update class

--- adviseme-webapp/src/app/pages/classes/edit-course/edit-course.component.ts
+++ adviseme-webapp/src/app/pages/classes/edit-course/edit-course.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 
 import { Class } from '../../../_shared/models/class';
+import { ClassService } from '../../../_shared/services/class.service';
+
+import { flattenObject } from '../courses/flattenObject';
 
 
 /**
@@ -15,22 +18,115 @@ import { Class } from '../../../_shared/models/class';
 
 export class EditCourseComponent implements OnInit {
 
-    /**
-      The data that will go into the table
-    */
-    source: LocalDataSource = new LocalDataSource();
+  prerequisites: string[];
+  curriculum_one: string[];
+  curriculum_two: string[][];
 
-    /**
-      Initializes new names for the imports
-    */
-    constructor() {
+  /**
+    Configuration for the table
+  */
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmSave: true,
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      class_prefix: {
+        title: 'Department',
+      },
+      class_courseNo: {
+        title: 'Course Number',
+      },
+      class_title: {
+        title: 'Course Title',
+      },
+    },
+  };
+
+  /**
+    The data that will go into the table
+  */
+  source: LocalDataSource = new LocalDataSource();
+
+  /**
+    Initializes new names for the imports
+  */
+  constructor(private classService: ClassService, private selectedClass: Class) {
+    this.prerequisites = [];
+    this.curriculum_one = [];
+    this.curriculum_two = [];
+  }
+
+  /**
+      Gets all the classes flattens the object to add to the table
+      @returns {none}
+  */
+  ngOnInit() {
+
+    this.classService.getClasses()
+      .subscribe((res: Class[]) => {
+        console.log(res);
+        this.source.load(flattenObject(res));
+      });
+  }
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete' + event.data.class_title + ' ?')) {
+      alert('Deleted' + event.data.class_title);
+      event.confirm.resolve();
+    } else {
+      alert('Aborted delete');
+      event.confirm.reject();
     }
+  }
+  onSaveConfirm(event): void {
 
-    /**
-        Gets all the classes flattens the object to add to the table
-        @returns {none}
-    */
-    ngOnInit() {
+    this.selectedClass._id = event.newData._id;
+    this.selectedClass.deapartment = event.newData.department;
 
+    this.selectedClass.class = {
+      title: String(event.newData.class_title),
+      courseNo: String(event.newData.class_courseNo),
+      prefix: String(event.newData.class_prefix),
+    };
+
+    Object.entries(event.newData).forEach(([key, value]) => {
+
+      if (key.startsWith('prerequisites_')) {
+        this.prerequisites.push(value);
+      }
+
+      if (key.startsWith('curriculum_')) {
+        this.curriculum_one.push(value);
+
+        if (this.curriculum_one.length === 2) {
+          this.curriculum_two.push([this.curriculum_one[0], this.curriculum_one[1]]);
+          this.curriculum_one.pop();
+          this.curriculum_one.pop();
+        }
+
+      }
+
+    });
+    this.selectedClass.prerequisites = this.prerequisites;
+    this.selectedClass.curriculum = this.curriculum_two;
+
+    if (window.confirm('Are you sure you want to edit ' + event.data.class_title + ' ?')) {
+      alert('Changes made');
+      event.confirm.resolve();
+    } else {
+      alert('Aborted changes');
+      event.confirm.reject();
     }
+  }
 }
