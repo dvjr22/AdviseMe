48ed7dc9a892e0a8d99f9581f51c6759c27c3610
Tyler Moon <moon.tyler@gmail.com>
2018-03-01 17:34:14 -0500

    Wired up advisor appointments screen the same as the students for the time being

--- adviseme-webapp/src/app/pages/advisor/appointments/appointments.component.ts
+++ adviseme-webapp/src/app/pages/advisor/appointments/appointments.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../../../_shared/models/appointment';
+import { AppointmentService } from '../../../_shared/services/appointment.service';
 
 @Component({
-  selector: 'app-appointments',
+  selector: 'ngx-app-appointments',
   templateUrl: './appointments.component.html',
-  styleUrls: ['./appointments.component.scss']
+  styleUrls: ['./appointments.component.scss'],
 })
 export class AppointmentsComponent implements OnInit {
+  /**
+   Variable to store all the appointments for the user collected from the api
+  */
+  appointment: any;
 
-  constructor() { }
-
+  /**
+    Initializes new names for the imports
+  */
+  constructor(private appointmentService: AppointmentService) {
+  }
+  /**
+    Calling the appointment api gets all the appointments
+    TODO: Get only appointments for the advisor, still being able to sort
+    @returns {none}
+  */
   ngOnInit() {
+    this.appointmentService.getAll().subscribe( res => {
+      this.appointment = res['data'];
+    });
   }
 
 }
