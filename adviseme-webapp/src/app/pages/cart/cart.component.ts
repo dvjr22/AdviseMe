90eb55a52945fa4c9a7933e8ffe5cbd0f3f77824
tbhall <tbhall@email.sc.edu>
2018-03-05 19:02:18 -0500

    request button blue, cart actions look good

--- adviseme-webapp/src/app/pages/cart/cart.component.ts
+++ adviseme-webapp/src/app/pages/cart/cart.component.ts
@@ -17,6 +17,22 @@ export class CartComponent implements OnInit {
   currentUser: User;
   // configuration for the table
   settings= {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmCreate: true,
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmSave: true,
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
     columns: {
       title: {
         title: 'Title',
