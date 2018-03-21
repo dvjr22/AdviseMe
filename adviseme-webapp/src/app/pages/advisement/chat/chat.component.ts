29205653c044e55ad03fd42738eb1ba36fe0d619
Tyler Moon <moon.tyler@gmail.com>
2018-03-20 22:45:02 -0400

    Replaced the hardcoded user with the current user model

--- adviseme-webapp/src/app/pages/advisement/chat/chat.component.ts
+++ adviseme-webapp/src/app/pages/advisement/chat/chat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { ChatService } from '../../../_shared/services/chat.service';
+import { UserService } from '../../../_shared/services/user.service';
+import { User } from '../../../_shared/models/user';
 import * as io from 'socket.io-client';
 
 @Component({
@@ -23,10 +25,11 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   socket = io('http://localhost:4001');
 
   // Not sure what this is for :(k
-  joinned = false;
+  joinned = true;
 
+  currentUser: User;
 
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService, private userService: UserService) {}
 
   ngOnInit() {
 
@@ -35,29 +38,40 @@ export class ChatComponent implements OnInit, AfterViewChecked {
       room: 'test',
       nickname: 'batman',
     };
+    this.currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    this.userService.getById(this.currentUser._id).subscribe((res) => {
+      this.currentUser = res;
+      console.log(res)
 
-    if (user !== null) {
-      // Get the chat room for the current user
-      this.getChatByRoom(user.room);
-      // Set the message data with the current users information
-      this.msgData = { room: user.room, nickname: user.nickname, message: '' };
-      // Trip the joinned flag
-      this.joinned = true;
-      // Scroll the message view to the bottom
-      this.scrollToBottom();
-    }
+      if (this.currentUser !== null && this.currentUser !== undefined) {
+        // Get the chat room for the current user
+        console.log(this.currentUser)
+        // Set the message data with the current users information
+        this.msgData = { room: this.currentUser.studentID, nickname: this.currentUser.studentID, message: '' };
+        // Trip the joinned flag
+        this.joinned = true;
+        // Scroll the message view to the bottom
+        this.scrollToBottom();
+      }
+    });
+    this.joinRoom()
     // Recieve the new message push from the socket server
     this.socket.on('new-message', function (data) {
       console.log('recieved message: ', data)
       // If the message belongs in this room then display it
       // TODO: Check for the right room
-      //  if (data.message.room === user.room) {
-        // Push the message onto the chats object
-        this.chats.push(data.message.data);
-        console.log('this.chats',this.chats)
-        this.msgData = { room: user.room, nickname: user.nickname, message: '' };
-      //  }
+      console.log('userdata', this.currentUser, data.message)
+        if (data.message.data !== undefined && data.message.data.room === this.currentUser.studentID) {
+          // Push the message onto the chats object
+          this.chats.push(data.message.data);
+          console.log('this.chats',this.chats)
+          this.msgData = { room: this.currentUser.studentID, nickname: this.currentUser.studentID, message: '' };
+        }
     }.bind(this));
+  }
+
+  ngAfterInit() {
+
   }
 
   ngAfterViewChecked() {
@@ -73,15 +87,21 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   getChatByRoom(room) {
     this.chatService.getChatByRoom(room).then((res) => {
       this.chats = res;
-    }, (err) => { });
+    }, (err) => { console.log(err) });
   }
 
   joinRoom() {
     const date = new Date();
-    this.getChatByRoom('test'); // TODO: Replace this with a user service call
-    this.msgData = { room: 'test', nickname: 'batman', message: '' };
-    this.joinned = true;
-    this.socket.emit('save-message', { room: 'test', nickname: 'batman', message: 'Join this room', updated_at: date });
+    this.userService.getById(this.currentUser._id).subscribe((res) => {
+      this.currentUser = res;
+      // if (this.currentUser !== undefined) {
+        this.getChatByRoom(this.currentUser.studentID); // TODO: Replace this with a user service call
+        this.msgData = { room: this.currentUser.studentID, nickname: this.currentUser.studentID, message: '' };
+        this.joinned = true;
+        this.socket.emit('save-message', { room: this.currentUser.studentID,
+          nickname: this.currentUser.studentID, message: 'Join this room', updated_at: date });
+      // }
+    });
   }
 
   sendMessage() {
@@ -90,9 +110,9 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     }, (err) => { });
   }
 
-  logout() {
-    const date = new Date();
-    this.socket.emit('save-message', { room: 'test', nickname: 'batman', message: 'Left this room', updated_at: date });
-    this.joinned = false;
-  }
+  // logout() {
+  //   const date = new Date();
+  //   this.socket.emit('save-message', { room: 'test', nickname: 'batman', message: 'Left this room', updated_at: date });
+  //   this.joinned = false;
+  // }
 }
