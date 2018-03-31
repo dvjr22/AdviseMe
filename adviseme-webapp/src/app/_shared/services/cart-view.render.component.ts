70091773e15f9eba047e95a3b36877dd83c8c2ac
tbhall <tbhall@email.sc.edu>
2018-03-31 13:12:24 -0400

    advisor request button color change to info

--- adviseme-webapp/src/app/_shared/services/cart-view.render.component.ts
+++ adviseme-webapp/src/app/_shared/services/cart-view.render.component.ts
@@ -5,10 +5,10 @@ import { Router } from '@angular/router';
 @Component({
   template: `
     <div class="btn-group btn-group-full-width" role="group">
-          <button  (click)="goClassView()" type="button" class="btn btn-success btn-group-icon btn-group-divider btn-fixed">
+          <button  (click)="goClassView()" type="button" class="btn btn-info btn-group-icon btn-group-divider btn-fixed">
             <i class="ion-information-circled"></i>
           </button>
-          <button (click)="goClassView()" type="button" class="btn btn-success">View Requested Classes</button>
+          <button (click)="goClassView()" type="button" class="btn btn-info">View Requested Classes</button>
   </div>
   `,
 })
