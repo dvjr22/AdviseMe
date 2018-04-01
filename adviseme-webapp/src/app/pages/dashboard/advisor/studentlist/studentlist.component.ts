0bd099a39c1e9669228c4b17bb6b6e5c6f3f419a
alcamech <alcamech@gmail.com>
2018-04-01 01:17:11 -0400

    added studentlist ts

---
+++ adviseme-webapp/src/app/pages/dashboard/advisor/studentlist/studentlist.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+import { flattenObject } from '../../../../_shared/scripts/flattenObject';
+import { User } from '../../../../_shared/models/user';
+import { UserService } from '../../../../_shared/services/user.service';
+
+@Component({
+  selector: 'ngx-studentlist',
+  templateUrl: './studentlist.component.html',
+})
+export class StudentlistComponent implements OnInit {
+
+
+  // Config for the table
+  settings= {
+  actions: false,
+  columns: {
+    _id: {
+      title: 'Student ID',
+    },
+    firstName: {
+      title: 'First Name',
+    },
+    lastName: {
+      title: 'Last Name',
+    },
+  },
+};
+
+source: LocalDataSource = new LocalDataSource();
+
+constructor(private userService: UserService) { }
+
+ngOnInit() {
+  this.userService.getCurrentUser().subscribe(res => {
+    const students = [];
+    const data = [
+      {
+        _id: '1',
+        firstName: 'l1',
+        lastName: 'l2',
+      },
+    ];
+    if (res['role'] === 'advisor') {
+      for (let i = 0; i < res['students'].length; i++) {
+        this.userService.getById(res['students'][i]).subscribe(res2 => {
+          students.push({
+            _id: res2['_id'],
+            firstName: res2['firstName'],
+            lastName: res2['lastName'],
+          });
+        });
+      }
+      this.source.load(students);
+    } else {
+      // do nothing
+    }
+  });
+}
+
+}
