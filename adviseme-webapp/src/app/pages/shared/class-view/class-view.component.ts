7765db6c67416cb009b046c1833287c2e7c389f4
ethangamma24 <ethanharmon2018@gmail.com>
2018-04-08 15:37:44 -0400

    Added prereqs table and removed related classes card.

--- adviseme-webapp/src/app/pages/shared/class-view/class-view.component.ts
+++ adviseme-webapp/src/app/pages/shared/class-view/class-view.component.ts
@@ -5,6 +5,10 @@ import { CapitalizePipe } from '../../../@theme/pipes/capitalize.pipe';
 import { Class } from '../../../_shared/models/class';
 import { ClassService } from '../../../_shared/services/class.service';
 
+import { Ng2SmartTableComponent } from 'ng2-smart-table/ng2-smart-table.component';
+import { Row } from 'ng2-smart-table/lib/data-set/row';
+import { LocalDataSource } from 'ng2-smart-table';
+
 import { Router } from '@angular/router';
 
 import {Location} from '@angular/common';
@@ -26,10 +30,30 @@ export class ClassViewComponent implements OnInit {
   _id;
   pastUrl;
   Class;
-
+  prerequisites;
 
   constructor(protected route: ActivatedRoute, private classService: ClassService, protected router: Router, private _location: Location) {
   }
+
+  /**
+    Configuration for the table
+  */
+  settings = {
+    actions: false,
+    pager: {
+      display: false,
+    },
+    columns: {
+      prerequisites: {
+        title: 'Prerequisites',
+      },
+    },
+  };
+
+  /**
+    The data that will go into the table
+  */
+  source: LocalDataSource = new LocalDataSource();
 
   /**
     Gets the currents users id from the local cache then uses the user service
@@ -41,7 +65,11 @@ export class ClassViewComponent implements OnInit {
     this.classService.getClass(this._id)
       .subscribe((res: Class) => {
         this.Class = res;
+        //this.source.load(res);
+        this.prerequisites = res.prerequisites;
       });
+
+
   }
   /**
     Goes back to the last location
