6435b54e04cd3e7978dde3cf2fbc662feaeeaf71
Tyler Moon <moon.tyler@gmail.com>
2018-04-19 18:22:37 -0400

    Added view tutorial button

--- adviseme-webapp/src/app/pages/shared/profile-view/profile-view.component.ts
+++ adviseme-webapp/src/app/pages/shared/profile-view/profile-view.component.ts
@@ -9,6 +9,7 @@ import {CapitalizePipe} from '../../../@theme/pipes/capitalize.pipe';
 import { MessageService } from 'primeng/components/common/messageservice';
 
 import { FileUploader } from 'ng2-file-upload/ng2-file-upload';
+import { Router } from '@angular/router';
 
 const URL = '/upload';
 /**
@@ -63,7 +64,8 @@ export class ProfileViewComponent implements OnInit {
   */
   constructor(private userService: UserService,
     private messageService: MessageService,
-    private cacheService: CacheService) {
+    private cacheService: CacheService,
+    private router: Router) {
   }
   /**
     Gets the currents users id from the local cache then uses the user service
@@ -122,6 +124,12 @@ export class ProfileViewComponent implements OnInit {
   values = '';
   onKey(event: any) { // without type info
     this.values = event.target.value;
+  }
+
+  redoTutorial(event) {
+    this.currentUser.tutorialEnabled = true;
+    this.userService.update(this.currentUser).subscribe(() => {});
+    this.router.navigate(['/']);
   }
 
 }
